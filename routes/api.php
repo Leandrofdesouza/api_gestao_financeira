0028632865695b41639959226e861cf55a835c28
Leandro Furtado de Souza <desouza321@gmail.com>
2022-12-09 16:17:58 -0300

    segundo commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', function(Request $request){
+    return "Olá Mundo!!";
+});
+
+Route::get('/teste', function(Request $request){
+    return "Olá Mundo teste";
+});
+
+Route::post('/teste', function(Request $request){
+    return "Olá Mundo teste no método POST";
+});
